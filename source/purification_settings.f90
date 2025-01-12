58ee47b8bcea3fe0405bc95e2746cce32973d7c3
AlbKat <akatbashev@gmail.com>
2025-01-12 13:43:19 +0100

    purification_settings(feat): add file to handle purification settings
    Signed-off-by: AlbKat <akatbashev@gmail.com>

---
+++ source/purification_settings.f90
@@ -0,0 +1,11 @@
+module purification_settings
+   implicit none
+   public :: initialize_purification
+   private
+contains
+   subroutine initialize_purification
+
+
+   end subroutine initialize_purification
+
+end module purification_settings
